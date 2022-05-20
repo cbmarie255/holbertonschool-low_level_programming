12ab4cc12b0d305a42518e17d0072f8e037567f3
cbmarie255 <court.b1993@gmail.com>
2022-05-20 11:46:24 -0700

    AHHHHHHHHH

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - printing a message
+ * main - Initiate 
  *
- * return - 0
+ * return - Always 0
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
+
 }
