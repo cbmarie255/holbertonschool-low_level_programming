0fc83f8d46acbb797f6574f8a1b62fbd4207c714
cbmarie255 <court.b1993@gmail.com>
2022-05-20 12:21:35 -0700

    I can NOT

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - will display a message
+ * main - Initiate
  *
- * Return - Always 0
+ * Return : Always 0
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
