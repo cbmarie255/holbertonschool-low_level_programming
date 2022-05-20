2c13128254b3dda9c733b85f902f5802443bfbea
cbmarie255 <court.b1993@gmail.com>
2022-05-20 12:50:51 -0700

    Last one PUSH THROUGH

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- Will print all bytes of all files
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char charType;
+int intType;
+long int longintType;
+long long int longlongintType;
+float floatType;
+printf("Size of a char: %zu bytes\n", sizeof(charType));
+printf("Size of an int: %zu bytes\n", sizeof(intType));
+printf("Size of a long int: %zu bytes\n", sizeof(longintType));
+printf("Size of a long long int: %zu byte\n", sizeof(longlongintType));
+printf("Size of a float: %zu bytes\n", sizeof(floatType));
+return (0);
+}
