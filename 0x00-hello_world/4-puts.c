64199ebe1514ca123308a6119bebe7edd6c23291
cbmarie255 <court.b1993@gmail.com>
2022-05-20 10:51:24 -0700

    Come onnnnn!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
+#include<string.h>
 int main () {
-	fputs (0x22) 
-        puts ("Programming is like bulding a multilingual puzzle");
-return 0;
+	char str1[15];
+	char str2[15];
+
+	strcpy(str1, 0x22);
+	strcpy(str2, "Programming is like building a multilingual puzzle");
+	
+	puts(str1);
+        fputs(str2);
+	
+	return 0;
 }
