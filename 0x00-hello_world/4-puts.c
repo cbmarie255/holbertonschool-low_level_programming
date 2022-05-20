abf6827b8fddfdfbad0b84d11d34047139b2ade4
cbmarie255 <court.b1993@gmail.com>
2022-05-20 09:41:19 -0700

    Oh C... we have a date this weekend

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-puts ("Programming is like building a multilingual puzzle")
+int main () {
+	puts (0x22) 
+       puts ("Programming is like bulding a multilingual puzzle");
+getchar ();
+return 0;
+}
