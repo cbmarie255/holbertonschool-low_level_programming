39b36b15bd1a5cf78449febf34539614ab25f4bf
cbmarie255 <court.b1993@gmail.com>
2022-05-20 11:50:07 -0700

    PLEASEEEEE

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - Initiate 
  *
- * return - Always 0
+ * Return: Always 0
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
 
+return (0);
 }
