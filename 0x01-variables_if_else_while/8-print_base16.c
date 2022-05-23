7fe511b551f8726dcf993a7d572e1ea7485a6aa1
cbmarie255 <court.b1993@gmail.com>
2022-05-23 14:24:21 -0700

    Last one

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 /**
- * main- This script will print base 16 in hexidecimal
+ * main- This script will print hexidecimal 16
  *
  * Return: Always 0 (sucessful)
  */
 int main(void)
 {
-char ch
-for (ch = '0' ; ch >= 0x16 ; ch++)
+int x;
+char ch;
+for (x = '0' ; x <= '9' ; x++)
 {
-putchar("%x");
+putchar(x);
+}
+for (ch = 'a' ; ch <= 'f' ; ch++)
+{
+putchar(ch);
 }
 putchar('\n');
 return (0);
