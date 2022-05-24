cf231e25c41e61af0e0d77cd9e6194b2c03bb4ec
cbmarie255 <court.b1993@gmail.com>
2022-05-24 16:57:23 -0700

    ...

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main"
+/**
+ *print_last_digit - print the ones place
+ *@n: n is the generated number
+ *Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+for (digit = n % 10)
+{
+return(digit);
+}
