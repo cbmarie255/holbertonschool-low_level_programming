5ecbb93b9a7b1d84cc81154881ed8fe4cbec90c2
cbmarie255 <court.b1993@gmail.com>
2022-05-24 12:47:12 -0700

    am I slow orrrr

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet
+ * print_alphabet - prints lowercase alphabet
  *
  * Return: Always 0.
  */
 void print_alphabet(void);
-int alpha = 0;
 {
+int alpha
 for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
 _putchar(alpha);
