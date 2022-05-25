2bda9ea0658d60a38d35467778f4a0ea571faaa1
cbmarie255 <court.b1993@gmail.com>
2022-05-25 06:54:44 -0700

    thanks Matt

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,10 +7,48 @@
  */
 void print_to_98(int n)
 {
-for (n = 0; n < 98; n++)
+if (n <= 98)
+{
+for (n = n; n <= 98; n++)
+{
+if (n <= 9)
 {
 _putchar(n + 48);
 _putchar(',');
 _putchar(' ');
+}
+else 
+{
+_putchar((n / 10) + 48);
+_putchar((n % 10) + 48);
+if (n != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+}
+else 
+{
+for (n = n; n <= 98; n++)
+{
+if (n <= 9)
+{
+_putchar(n + 48);
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar((n / 10) + 48);
+_putchar((n % 10) + 48);
+if (n != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
 }
 }
