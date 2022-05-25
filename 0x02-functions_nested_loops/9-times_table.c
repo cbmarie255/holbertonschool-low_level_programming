50276b08b67f4a3252554117b7fc052af6f93ed0
cbmarie255 <court.b1993@gmail.com>
2022-05-24 20:52:15 -0700

    Times table in C

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *times_table - will print an oldschool times table
+ *
+ * Return: Always 0
+ */
+void times_table(void)
+{
+int c = 0;
+int r = 0;
+int p = 0;
+
+for (c = 0; c < 9; c++ && r = 0; r < 9; r++)
+{
+while ((r * c = p))
+{
+_putchar((p / 10) + '0');
+_putchar((p % 10) + '0');
+_putchar(',');
+_putchar(' ');
+_putchar('\n');
+}
+}
+}
