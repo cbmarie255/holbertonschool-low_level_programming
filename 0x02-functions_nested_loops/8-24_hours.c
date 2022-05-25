f5d77db8f2d4d62ef71fe76c7fca1a66388e203f
cbmarie255 <court.b1993@gmail.com>
2022-05-24 18:18:36 -0700

    worst project dawg

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - printing every minute of the day
+ *@h: hour
+ @m: minute
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+int h = 0
+int m = 0 
+for (h = 0; h < 24; h++)
+{
+for (m = 0; h < 60; m++)
+{
+_putchar((h/10) + '0');
+_putchar((h%10) + '0');
+_putchar(':');
+_putchar((m/10) + '0');
+_putchar((m%10) + '0');
+_putchar('\n');
+}
+return (0);
+}
+}
