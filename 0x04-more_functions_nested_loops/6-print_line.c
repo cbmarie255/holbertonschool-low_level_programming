5165c7fcc950df9e28f35b75cc7b54fe40fd1e87
cbmarie255 <court.b1993@gmail.com>
2022-05-30 13:01:42 -0700

    trying this

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - will print an underscore for n times
+ * @n: number provided by promt to run script
+ * Return: Always 0 for success
+ */
+void print_line(int n)
+{
+int n;
+char c = _;
+for (n = c)
+{
+_putchar(c + 95);
+_putchar(92);
+_putchar(110);
+_putchar('\n');
+if (n <= 0)
+{
+_putchar(92);
+_putchar(110);
+}
+}
+}
