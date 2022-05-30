de7c2e52d173979ce2816163e99975dea8b1787e
cbmarie255 <court.b1993@gmail.com>
2022-05-30 12:35:27 -0700

    Printing repeated data

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,16 @@
 void more_numbers(void)
 {
 int n, t;
-for (t = 1; t <=10; t++)
+for (t = 1; t <= 10; t++)
 {
 for (n = 0; n <= 14; n++)
+{
 if (n > 9)
 {
-_putchar((n % 10) + '0');
-}
 _putchar((n / 10) + '0');
 }
+_putchar((n % 10) + '0');
+}
 _putchar('\n');
+}
 }
--- 0x04-more_functions_nested_loops/text.c
+++ 0x04-more_functions_nested_loops/text.c
@@ -3,15 +3,16 @@
 int main(void)
 {
 int n, t;
-for (t = 1; t <=10; t++)
+for (t = 1; t <= 10; t++)
 {
 for (n = 0; n <= 14; n++)
 {
-if (n > 9)
+if (n > 9) 
 {
+putchar((n / 10) + '0');
+}
 putchar((n % 10) + '0');
 }
-putchar((n / 10) + '0');
 putchar('\n');
 }
 }
