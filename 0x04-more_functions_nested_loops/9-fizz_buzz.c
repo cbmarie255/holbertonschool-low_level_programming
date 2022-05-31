e60cccc53dd95f51702ece3dd7fde042e19c38f0
cbmarie255 <court.b1993@gmail.com>
2022-05-31 09:41:04 -0700

    Rewriting for certain variables

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *main - using for standard library
+ *
+ *Return: 0 for success
+ */
+int main(void)
+{
+int x;
+
+for (x = 1; x <= 100; x++)
+{
+if (x % 3 == 0 && x % 5 == 0)
+{
+printf("FizzBuzz");
+}
+if (x % 3 == 0)
+{
+printf("Fizz");
+}
+if (x % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", x);
+}
+printf("\n");
+}
+return (0);
+}
