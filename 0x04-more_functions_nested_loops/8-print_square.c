a2a297291929519b1c3be0f18a8e9359f09446f6
cbmarie255 <court.b1993@gmail.com>
2022-05-31 08:52:53 -0700

    Printing squares in C using putchar

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - will print a character in diagonal
+ * @size: is the variable that will change depended on the number fed to loop
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+int size;
+int s;
+int l;
+
+if (size > 0)
+{
+for (s = 1; s <= size; s++)
+{
+for (l = 1; l <= size; l++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+}
