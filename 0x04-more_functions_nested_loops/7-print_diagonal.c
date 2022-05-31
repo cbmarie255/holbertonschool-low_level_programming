0e5ac476db404e055d69d88cfb5429482f8b9a06
cbmarie255 <court.b1993@gmail.com>
2022-05-31 08:14:50 -0700

    Printing a pattern in putchar

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - will print a character in diagonal
+ * @n: is the variable that will change depended on the number fed to loop
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+int d;
+int s;
+
+if (n > 0)
+{
+for (d = 0; d < n; d++)
+{
+for (s = 1; s < d; s++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+}
+}
