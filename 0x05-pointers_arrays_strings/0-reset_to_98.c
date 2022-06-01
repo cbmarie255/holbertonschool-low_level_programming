09da7b7eaba444803bdcbbe69d5b97f29609e4de
cbmarie255 <court.b1993@gmail.com>
2022-06-01 07:41:04 -0700

    Pointer pointing to another value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *reset_to_98- a function that takes a pointer and updates its value
+ *@*n: refers to the pointer
+ *Return: null for success
+ */
+void reset_to_98(int *n)
+{
+int s = 98;
+int *n = &s;
+}
