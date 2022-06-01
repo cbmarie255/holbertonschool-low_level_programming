a65e5d09bbecb23fd76b13964288bfed9e226d92
cbmarie255 <court.b1993@gmail.com>
2022-06-01 11:57:38 -0700

    Writing a function simular to puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts - function that prints string to stdout
+ *@str: represents a string
+ *Return: always null for success
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+str++;
+_putchar(*str);
+}
+_putchar('\n');
+}
