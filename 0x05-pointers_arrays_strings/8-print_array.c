4df1e31f314ff84788730a89ed64ff402eddc9bf
cbmarie255 <court.b1993@gmail.com>
2022-06-02 11:24:52 -0700

    Printing the elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,9 +10,9 @@ void print_array(int *a, int n)
 {
 int b;
 
-for (b = 0; b < (n - 1); b++)
+for (b = 0; b < n; b++)
 {
-printf("%d, ", a[b]); 
+printf("%d, ", a[b]);
 }
 for (b = (n - 1); b < n; b++)
 {
