8da950dbd703b703ea4f3a596fa77419b371916f
cbmarie255 <court.b1993@gmail.com>
2022-06-02 12:24:48 -0700

    Returning a strings new destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_strcpy - will return a copy of a string
+ *@dest: new destination
+ *@src: is the data destination
+ *Return: always null for success
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *temp = dest;
+int s = 0;
+{
+while (src [s] != '\0')
+{
+*temp = *src;
+s++;
+dest++;
+}
+*dest = '\0';
+return (temp);
+}
+}
