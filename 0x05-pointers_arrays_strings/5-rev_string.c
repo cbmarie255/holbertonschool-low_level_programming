81a031f79c5b4d28662bd23937affaaee2387586
cbmarie255 <court.b1993@gmail.com>
2022-06-02 07:31:11 -0700

    Reversing a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,13 +8,13 @@ void rev_string(char *s)
 {
 int b;
 int c = 0;
-int e = 0;
+int e = *c
 
 while (s[c] != '\0')
 {
 c++;
 }
-for (b = 0; b >= (c - 1); b++)
+for (b = 0; b <= (c - 1); b--)
 {
 e--;
 }
