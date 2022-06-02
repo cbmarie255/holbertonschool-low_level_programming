1c3fb8fccc7657106162a3ed1e88cb580d80b126
cbmarie255 <court.b1993@gmail.com>
2022-06-02 07:08:21 -0700

    Reversing a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *puts2 - using function to reverse a string
+ *rev_string - using function to reverse a string
  *@s: value representing the string
  *Return: always null for success
  */
@@ -14,10 +14,10 @@ while (s[c] != '\0')
 {
 c++;
 }
-for (b =0; b < c; b++)
+for (b =0; b < c; b--)
 {
 e--;
-_putchar(s[e]);
+_putchar(s[c]);
 }
 _putchar ('\n');
 }
