785e4c97518037afbec6bd4299aa2e09229fb241
cbmarie255 <court.b1993@gmail.com>
2022-06-02 09:43:18 -0700

    Printing the second half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,14 +15,14 @@ c++;
 }
 if (c % 2 == 0)
 {
-for ((h = (c / 2)); h > c; h++)
+for ((h = (c / 2)); h < c; h++)
 {
 _putchar(str[h]);
 }
 }
-else  
+else
 {
-for (((h = (c / 2) + 1 )); h > c; h++)
+for (((h = (c / 2) + 1)); h < c; h++)
 {
 _putchar(str[h]);
 }
