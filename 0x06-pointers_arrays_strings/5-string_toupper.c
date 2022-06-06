6f737a0135074bc732594b8d5bb2481720688ae5
cbmarie255 <court.b1993@gmail.com>
2022-06-06 15:29:31 -0700

    Changes all lowercase letter to uppercase in a string

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,15 +5,16 @@
  *
  *Return: always null for success
  */
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
 int i;
 
-for (i = 0; *[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-if (*[i] >= 'a' && *[i] <= 'z')
+if (s[i] >= 'a' && s[i] <= 'z')
 {
-*[i] = *[i] - 32;
+s[i] = s[i] - 32;
 }
 }
+return (s);
 }
