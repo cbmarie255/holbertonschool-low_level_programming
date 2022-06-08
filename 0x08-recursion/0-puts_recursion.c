75847f20d184f3ace5e9532471880c18604a2250
cbmarie255 <court.b1993@gmail.com>
2022-06-08 09:22:43 -0700

    Using puts in recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_puts_recursion -  will print a string followed by a new line
+ *@s: character string given by prompt
+ *Return: null or 0 for success
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_putchar(*s++);
+	}
+	_puts_recursion(s + 1);
+	_putchar('\n');
+}
