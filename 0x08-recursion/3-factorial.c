f62e47f16fb77696a1d713c9c9805b829b43c6a5
cbmarie255 <court.b1993@gmail.com>
2022-06-08 11:48:28 -0700

    Printing the factorial of an int

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *factorial - will return the factorial of a given number, n
+ *@n: int value provided by prompt
+ *Return: either -1 or factorial value, depending on case
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
