7be9ec4f669c0e13c77d2244d1559cd660aa39bb
cbmarie255 <court.b1993@gmail.com>
2022-06-09 12:12:20 -0700

    updates

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *leet - will replace certain characters
+ *@s: string given by prompt
+ *Return: replacement character
+ */
+char *leet(char *s)
+{
+	int k = 0;
+	char *replacement = s;
+	char *letter = "aAeEoOtTlL";
+	char *number = "4433007711";
+
+	while (*s)
+	{
+		for (k = 0; letter[k]; k++)
+		{
+			*s = number[k];
+		}
+	s++;
+	}
+	return (replacement);
+}
