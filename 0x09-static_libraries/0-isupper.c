020074f4450b2d18a815a6ce61b06b5da3929d53
cbmarie255 <court.b1993@gmail.com>
2022-06-13 20:27:59 -0700

    Thought I had it on the first try this time :(

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - checks for an uppercase letter
+ *@c: character used to check for uppercase letter
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strcat - will mimic the function of strcat
+ *@dest: file to concatenate
+ *@src: string that will be appended to dest
+ *Return: always null for success
+ */
+char *_strcat(char *dest, char *src)
+{
+int k = 0;
+int f = 0;
+
+while (dest[k])
+{
+k++;
+}
+while (src[f])
+{
+dest[k] = src[f];
+f++;
+k++;
+}
+return (dest);
+}
