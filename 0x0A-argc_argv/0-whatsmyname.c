1a192df8a5b358b0ca4747654b7e682bd91bccae
cbmarie255 <court.b1993@gmail.com>
2022-06-14 18:37:50 -0700

    Project attempt #1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- *main - printing a program's name 
+ *main - printing a program's name
  *
  *Return: filename or 0
  */
 
-int main (int argc __attribute__ ((unused)), char *argv[])
+int main(int argc __attribute__ ((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
