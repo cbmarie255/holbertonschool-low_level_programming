26fe3389545bd8f104ee1fd25a61c7f842e7b46d
cbmarie255 <court.b1993@gmail.com>
2022-06-14 20:58:02 -0700

    ...

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  *
@@ -7,20 +8,13 @@
  *
  *
  */
-int main(void)
-{
-	int a, b, product;
-
-
-	scanf("%lf %lf", &a, &b);
-	
-	if (argc != 2)
+int main(int argc, char *argv[])
+{	
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	product = a * b;
-
-	printf("%lf", product);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
