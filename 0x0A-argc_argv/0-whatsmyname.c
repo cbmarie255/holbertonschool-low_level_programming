736d2e125ec57a2c65669a6de312f4bae1997723
cbmarie255 <court.b1993@gmail.com>
2022-06-14 18:34:14 -0700

    Project attempt #1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - printing a program's name 
+ *
+ *Return: filename or 0
+ */
+
+int main (int argc __attribute__ ((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory will explore the functional use and differences between argc & argv
