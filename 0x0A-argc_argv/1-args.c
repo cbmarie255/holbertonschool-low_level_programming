687204b78d8a770027d17afa4fb2cbc7df8e2ed0
cbmarie255 <court.b1993@gmail.com>
2022-06-14 20:40:33 -0700

    ...

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  *@argv: argument vector
  *Return: count
  */
-int main (int argc, char *argv[] __attribute__ ((unused)));
+int main (int argc, char *argv[] __attribute__ ((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
