16e63e8a8a27610d0cac4b0ceadb29b3592927b8
cbmarie255 <court.b1993@gmail.com>
2022-06-16 09:49:33 -0700

    First attempt

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - will return a pointer to a new string
+ *@str: original string
+ *Return: null or new string
+ */
+char *_strdup(char *str)
+{
+	char *duplicate;
+	char *holder;
+	int k = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[k])
+	{
+		k++;
+	}
+	duplicate = malloc(k + 1);
+	holder = duplicate;
+	while (*str)
+	{
+		*holder++ = *str++;
+	}
+	*holder = '\0';
+	return (duplicate);
+}
