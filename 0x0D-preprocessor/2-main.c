3f3919e3a44d8e3aed90f66cc7414de7cb778679
cbmarie255 <court.b1993@gmail.com>
2022-06-17 11:39:36 -0700

    Please, more of these projects!

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - will print the name of the file it was compiled from
+ *
+ *Return - the file name
+ */
+int main(void)
+{
+	printf("%d\n", _FILE_);
+	return (0);
+}
