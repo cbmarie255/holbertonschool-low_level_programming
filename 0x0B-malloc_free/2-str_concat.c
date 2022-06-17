f89e91d3996fd9ac61150ede995dd6462e7c7de6
cbmarie255 <court.b1993@gmail.com>
2022-06-16 21:31:29 -0700

    going to bed- late try before the deadline

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,11 +30,13 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	while (ex1 < len1)
 	{
-		len1 = len1 + '\0';
+		len1++; 
+		len1 = '\0';
 	}
 	while (ex2 < len2)
 	{
-		len2 = len2 + '\0';
+		len2++;
+		len2 = '\0';
 	}
 	return (s3);
 
