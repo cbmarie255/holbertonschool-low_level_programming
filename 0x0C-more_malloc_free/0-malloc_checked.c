d1b9646a4b6104d354b21e36d27a4f44021e532b
cbmarie255 <court.b1993@gmail.com>
2022-06-21 10:20:47 -0700

    correct output, failed betty!

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 
 /**
  *malloc_checked - will allocate memory using malloc
- *@b: 
+ *@b: variable with conditions, assigned by prompt
  *Return: always 0 for success or NULL
  */
 void *malloc_checked(unsigned int b)
@@ -12,7 +12,7 @@ void *malloc_checked(unsigned int b)
 
 	if (pointer == NULL)
 	{
-		exit (98);
+		exit(98);
 	}
 	return (pointer);
 }
