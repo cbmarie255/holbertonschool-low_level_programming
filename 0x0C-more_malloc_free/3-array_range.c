2dd18eda05b97c47c5773bc853a313ff44a1dd89
cbmarie255 <court.b1993@gmail.com>
2022-06-21 12:12:13 -0700

    adding another variable to initialize at 0

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,6 +11,7 @@ int *array_range(int min, int max)
 {
 	int *array;
 	int k;
+	int f = 0;
 
 	if (min > max)
 	{
@@ -18,12 +19,13 @@ int *array_range(int min, int max)
 	}
 	array = malloc(sizeof(int) * ((max - min) + 1));
 	if (array == NULL)
-        {
-                return (NULL);
-        }
+	{
+		return (NULL);
+	}
 	for (k = min; k <= max; k++)
 	{
 		array[k] = k;
+		f++;
 	}
 	return (array);
 }
