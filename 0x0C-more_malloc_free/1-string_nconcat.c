b4539dce95532bab340548b01b81f77abb6ff26f
cbmarie255 <court.b1993@gmail.com>
2022-06-21 10:33:58 -0700

    it compiled, not sure if it does what I want :(

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  *string_nconcat - will concatenate 2 strings
@@ -11,26 +10,37 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char new_array;
-	int k;
-	int f;
-	int length;
+	char *new_array;
+	unsigned int k = 0;
+	unsigned int f = 0;
+	unsigned int length;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
-	
+	}
 	while (s1[k])
+	{
 		k++;
-	while (s2[f]);
+	}
+	while (s2[f])
+	{
 		f++;
+	}
 	if (f >= n)
+	{
 		f = n;
+	}
 	length = k + f;
 
 	new_array = malloc(sizeof(char) * (length + 1));
 	if (new_array == NULL)
+	{
 		return (NULL);
+	}
 	return (new_array);
 }
