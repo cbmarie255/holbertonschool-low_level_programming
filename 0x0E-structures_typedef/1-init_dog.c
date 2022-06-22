84f1247debc76705d27189c416a5479d372e63ad
cbmarie255 <court.b1993@gmail.com>
2022-06-22 11:11:09 -0700

    duh Courtney you have to make the structs useable!

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ *init_dog - will initialize a variable of type struct dog
  *@d: pointer used for user
  *@name: name of dog
  *@age: age of dog
@@ -9,12 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		return (NULL);
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
-
-	d -> name;
-	d -> age;
-	d -> owner;
 }
