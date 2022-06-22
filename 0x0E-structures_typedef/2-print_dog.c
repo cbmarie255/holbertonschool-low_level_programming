8eb8e9fbb887bd971ed8eacb311ef24f69bf00b9
cbmarie255 <court.b1993@gmail.com>
2022-06-22 11:27:57 -0700

    more than 0, not less

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,8 +15,7 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)");
 		else
 			printf("Name: %s\n", d->name);
-
-		if (d->age > 0)
+		if (d->age < 0)
 			printf("Age: (nil)");
 		else
 			printf("Age: %f\n", d->age);
