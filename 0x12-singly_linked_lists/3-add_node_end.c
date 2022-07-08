2136fb00bb71f66713b5798bc4d3e2f52b9e26f5
cbmarie255 <court.b1993@gmail.com>
2022-07-08 08:44:36 -0700

    trying to free, seems simpler than 3!

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ *free_list - will free a list_t
+ *@head: pointer to the head of the list
+ *Return: void, freed list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	temp = head;
+	while (temp != NULL)
+	{
+		free(temp);
+		temp->next = temp;
+	}
