45ea3f80ec11e9ff14d19e8c3d5506a1988ce282
cbmarie255 <court.b1993@gmail.com>
2022-07-14 09:39:37 -0700

    trying to check another case

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free (temp);
 		return (1);
 	}
-	for (index_count = 0; index_count < (index - 1); index_count++)
+	for (index_count = 0; index_count <= (index - 1); index_count++)
 	{
 		temp = temp->next;
 		if (temp == NULL)
