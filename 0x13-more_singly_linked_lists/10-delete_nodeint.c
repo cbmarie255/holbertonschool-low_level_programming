27ea074663c2d8ae0dee47ac19ed5431f7012d91
cbmarie255 <court.b1993@gmail.com>
2022-07-14 09:47:55 -0700

    betty

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		temp = *head;
 		*head = temp->next;
-		free (temp);
+		free(temp);
 		return (1);
 	}
 	for (index_count = 0; index_count < (index - 1); index_count++)
