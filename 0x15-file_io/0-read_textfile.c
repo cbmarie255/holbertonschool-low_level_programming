32f219e174d8973953f92cf94e7380ce91db0054
cbmarie255 <court.b1993@gmail.com>
2022-07-18 12:51:18 -0700

    reading a textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *read_textfile - will read a text file and print it to the POSIX standard output
+ *@filename: pointer to the filename
+ *@letters: the actual number of letters it should read and print
+ *Return: the acutal number of letters it could read and print or 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor;
+	int read_it;
+	int write_it;
+	char *buffer = malloc(letters);
+
+	if (buffer == NULL || filename == NULL)
+		return (0);
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	read_it = read(file_descriptor, buffer, letters);
+	if (read_it < 0)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+	write_it = write(STDOUT_FILENO, buffer, read_it);
+	if (write_it < 0)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+	close(file_descriptor);
+	free(buffer);
+	return (write_it);
+}
+
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This directory will overview how to create, open, close, read, and write files in C. It will also demostrate how to use I/O system calls and file descriptors
