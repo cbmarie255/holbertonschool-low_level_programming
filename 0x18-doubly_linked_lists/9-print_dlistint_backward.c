6b1801bf545cf331cd0d7648615eeb291b65e075
cbmarie255 <court.b1993@gmail.com>
2022-08-02 10:58:37 -0700

    printing a doubly linked list backwards

---
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ *print_dlistint_backward - will print a doubly list backwards
+ *@h: pointer to the head of a list
+ *Return: node count or NULL
+ */
+size_t print_dlistint_backward(const dlistint_t *h)
+{
+	size_t node_count;
+	const dlistint_t *temp;
+
+	temp = h;
+	for (node_count = 0; temp->next != NULL; )
+	{
+		temp = temp->next;
+	}
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->prev;
+		node_count++;
+	}
+	return (node_count);
+}
