4ae0ddf7b2105cb94284542e260b85ddd4809949
cbmarie255 <court.b1993@gmail.com>
2022-08-04 21:33:32 -0700

    printing a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_print - will print a hash table
+ *@ht: pointer to hash table
+ *Return: void
+ */
+ void hash_table_print(const hash_table_t *ht)
+{
+	int commaSpace = 0;
+	hash_node_t *printNode;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+	putchar('{');
+	for (i = 0; i < ht->size; i++)
+	{
+		printNode = ht->array[i];
+		while (printNode != NULL)
+		{
+			if (commaSpace == 1)
+				printf(", ");
+			printf("'%s': '%s'", printNode->key, printNode->value);
+			commaSpace = 1;
+			printNode = printNode->next;
+		}
+	}
+	printf("}\n");
+}
