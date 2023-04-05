cf4f5d0471ee46bfb6d61acf4ad1a96693077f3b
cbmarie255 <court.b1993@gmail.com>
2023-04-05 11:42:49 -0500

    can't declare inline ughhhh

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -10,10 +10,12 @@
  */
 void print_array(const int *array, size_t size)
 {
+    size_t i = 0;
+
     if (!array)
         return;
 
-    for (size_t i = 0; i < size; i++) 
+    for (i = 0; i < size; i++)
     {
         if (i > 0)
         printf(", ");
