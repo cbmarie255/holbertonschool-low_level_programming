5a84389aeef0f0435e9338f17187ffe61888d333
cbmarie255 <court.b1993@gmail.com>
2023-04-05 10:45:28 -0500

    linear search merthod

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+/**
+ * linear_search - find a specific value in an array using linear search
+ * @array: pointer to first element
+ * @size: number of elements
+ * @value: searching value 
+ *
+ * Return: 1st index where value is located or -1 if not found or array is null
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	while (i < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
