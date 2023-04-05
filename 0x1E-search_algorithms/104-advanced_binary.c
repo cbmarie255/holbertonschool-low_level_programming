831b4db9bd868d3c3243aa28b2f1f5570eeabca5
cbmarie255 <court.b1993@gmail.com>
2023-04-05 11:41:13 -0500

    advanced binary search

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * print_array - Prints an array of ints
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ * Return: printed array
+ */
+void print_array(const int *array, size_t size)
+{
+    if (!array)
+        return;
+
+    for (size_t i = 0; i < size; i++) 
+    {
+        if (i > 0)
+        printf(", ");
+        printf("%d", array[i]);
+    }
+    printf("\n");
+}
+
+/**
+* advanced_binary - Searches for value using a binary search
+* @array: Pointer to the first element of array
+* @size: Number of elements
+* @value: Value to be searched
+* Return: Index of value or -1 for NULL
+**/
+int advanced_binary(int *array, size_t size, int value)
+{
+    int low = 0;
+    int high = size - 1;
+    int mid;
+
+    if (array == NULL)
+        return (-1);
+
+    while (low <= high) 
+    {
+        mid = (low + high) / 2;
+        printf("Searching in array: ");
+        print_array(array + low, high - low + 1);
+
+        if (array[mid] == value)
+            return (mid);
+
+        if (array[mid] < value)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -40,5 +40,7 @@ typedef struct skiplist_s
 int linear_search(int *array, size_t size, int value);
 void print_subarray(int *array, size_t left, size_t right);
 int binary_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+void print_array(const int *array, size_t size);
 
 #endif
