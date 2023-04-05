19fd346c1b3184f23ce7a151a2423d7e324acee0
cbmarie255 <court.b1993@gmail.com>
2023-04-05 11:45:19 -0500

    using recursion

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -25,34 +25,33 @@ void print_array(const int *array, size_t size)
 }
 
 /**
-* advanced_binary - Searches for value using a binary search
-* @array: Pointer to the first element of array
-* @size: Number of elements
-* @value: Value to be searched
-* Return: Index of value or -1 for NULL
-**/
+ * advanced_binary - searches for a value in a sorted array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in the array
+ * @value: value to search for
+ *
+ * Return: index of the value in the array, or -1 if not found
+ */
 int advanced_binary(int *array, size_t size, int value)
 {
-    int low = 0;
-    int high = size - 1;
-    int mid;
-
-    if (array == NULL)
+    if (array == NULL || size == 0)
         return (-1);
 
-    while (low <= high) 
+    int mid = (size - 1) / 2;
+
+    printf("Searching in array: ");
+    print_array(array, size);
+
+    if (array[mid] == value)
+        return (mid);
+
+    if (array[mid] < value)
     {
-        mid = (low + high) / 2;
-        printf("Searching in array: ");
-        print_array(array + low, high - low + 1);
-
-        if (array[mid] == value)
-            return (mid);
-
-        if (array[mid] < value)
-            low = mid + 1;
-        else
-            high = mid - 1;
+        int index = advanced_binary(array + mid + 1, size - mid - 1, value);
+        return (index == -1 ? -1 : mid + 1 + index);
     }
+    else
+        return advanced_binary(array, mid + 1, value);
+
     return (-1);
 }
